650bd804b93f660378f7000a35ff3826970d09f8
Ada64 <55141583+Ada64@users.noreply.github.com>
2025-07-01 15:04:21 +0300

    Add files via upload

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,3 +1,3 @@
 export async function onRequest(context) {
-  return Response.redirect("https://discord.com/oauth2/authorize?client_id=1389376514763526196&response_type=code&redirect_uri=++https%3A%2F%2Fmiguel-vote.pages.dev%2Fapi%2Fcallback&scope=identify", 302);
+  return Response.redirect("https://discord.com/oauth2/authorize?client_id=1389376514763526196&response_type=code&redirect_uri=https%3A%2F%2Fmiguel-vote.pages.dev%2Fapi%2Fcallback&scope=identify", 302);
 } 
